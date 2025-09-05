fa48a8308c2f9c8d45be035680cda6797afe710e
musa-bojang <musabojang413@gmail.com>
2025-09-05 08:47:43 +0100

    duplicate app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ height: '200px' ,padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+    );
+}
+
+export default MainContent;
