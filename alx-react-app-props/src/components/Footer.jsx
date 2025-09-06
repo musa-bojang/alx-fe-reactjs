a8a2eaf8f38b3acc5cec8dea720ab9bc8caec2de
musa-bojang <musabojang413@gmail.com>
2025-09-06 22:19:53 +0100

    task 2

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }}>
+        <p style={{color: 'black'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
