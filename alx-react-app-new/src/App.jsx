fb98e14643883407f440c87311da87cba1bc8a19
musa-bojang <musabojang413@gmail.com>
2025-09-06 22:12:45 +0100

    updates

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,17 +7,24 @@ import Header from './components/Header.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import UserContext from './context/UserContext.js'
+import ProfilePage from './components/ProfilePage.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const userData = { name: "Jane Doe 3", email: "jane.doe@example.com" };
   return (
     <>
     <WelcomeMessage />
     <Header />
     <MainContent />
     <Footer />
-    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+   
+   <UserProfile name="John Doe" age={30} bio="A software developer from NY." />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
