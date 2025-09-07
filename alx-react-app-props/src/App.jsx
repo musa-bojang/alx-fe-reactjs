9f872298f5d16b301223b841178b28a6825daad3
musa-bojang <musabojang413@gmail.com>
2025-09-07 19:38:10 +0100

    updates

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,16 +3,18 @@ import './App.css'
 
 import UserContext from "./components/UserContext.js"
 import ProfilePage from './components/ProfilePage.jsx'
+import UserProfile from './components/UserProfile.jsx';
 
 
 function App() {
   
-  const userData = { name: "Jane Doe 3", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe 3", email: "jane.doe@example.com", bio: "A passionate developer.", age: 28 };
   return (
     <>
     
     <UserContext.Provider value={userData}>
       <ProfilePage />
+   <UserProfile />
     </UserContext.Provider>
   
     </>
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-function UserProfile(props) {
+import UserContext from "./UserContext";
+import { useContext } from "react";
+
+
+function UserProfile() {
+    const data = useContext(UserContext);
     return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-   <h2 style={{ color: 'blue' }}>{props.name}</h2>
-   <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-   <p>Bio: {props.bio}</p>
+   <h2 style={{ color: 'blue' }}>{data.name}</h2>
+   <p>Age: <span style={{ fontWeight: 'bold' }}>{data.age}</span></p>
+   <p>Bio: {data.bio}</p>
  </div>
     );
 }
