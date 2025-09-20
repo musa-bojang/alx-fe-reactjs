c1665c53d3746fa0fc6d114b147650745aadc797
musa-bojang <musabojang413@gmail.com>
2025-09-20 11:03:49 +0100

    updaates

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -4,22 +4,39 @@ import { fetchUserData } from "../services/githubService";
 function Search() {
 const [query, setQuery] = useState("");
 const [newData, setNewdata] = useState("");
+const [loading, setLoading] = useState(false);
+const [error, setError ] = useState(null);
     function handleChange(e) {
         setQuery(e.target.value);
     }
-    useEffect(() => {
-        if(query.trim()==="") return;
-
-        fetchUserData(query)
-        .then((data) => setNewdata(data))
-        .catch((err) => console.error(err));
-      }, [query]);
+    async function handleSubmit(e) {
+        e.preventDefault(); // prevent page reload
+        if (query.trim() === "") return;
+    
+        setLoading(true);
+        setError(null);
+    
+        try {
+          const data = await fetchUserData(query);
+          setNewdata(data);
+        } catch (err) {
+          setError(err.message);
+          setNewdata(null);
+        } finally {
+          setLoading(false);
+        }
+      }
   return (
     <>
      <div>Search Component</div>
-    <input onChange={handleChange} style={{padding:"10px", color:"black", backgroundColor:"white"}} type="text" placeholder="type name of github user"/>
+     <form onSubmit={handleSubmit}>
+     <input onChange={handleChange} style={{padding:"10px", color:"black", backgroundColor:"white"}} type="text" placeholder="type name of github user"/>
+    <button type="submit" >search</button>
+     </form>
     
     <h2>results</h2>
+    {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
     {newData ? (
         <div>
           <p>👤 Username: {newData.login}</p>
