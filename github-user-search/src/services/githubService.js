0448a0b815a389faf26a14b857ba2d32a1af1dbd
musa-bojang <musabojang413@gmail.com>
2025-09-20 11:21:58 +0100

    url

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 
 const GITHUB_API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
-const GITHUB_API_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
+const GITHUB_API_URL = "https://api.github.com";
 import axios from "axios";
 
 const api = axios.create({
